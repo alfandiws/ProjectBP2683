5156771b76779528d2281da697bb96ea80a449fb
alfandiws <alfandiwicaksono@student.amikom.ac.id>
2023-09-27 16:05:01 +0700

    update

---
+++ app/src/main/java/com/example/projectbp2683/AboutActivity.kt
@@ -0,0 +1,16 @@
+package com.example.projectbp2683
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+        val imgCall:ImageView = findViewById(R.id.imageViewCall)
+        imgCall.setOnClickListener{
+            val callIntent:Intent
+        }
+    }
+}
---
+++ app/src/main/java/com/example/projectbp2683/LoginActivity.kt
@@ -0,0 +1,19 @@
+package com.example.projectbp2683
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        val btnLogin:Button =findViewById(R.id.buttonLogin)
+
+        btnLogin.setOnClickListener(){
+            val intenHome = Intent(this,HomeActivity::class.java)
+            startActivity(intenHome)
+        }
+    }
+}
