5a7c45d53f6683987e2d8144137d95a421a82a0c
amikom <alfandiwicaksono@students.amikom.ac.id>
2023-10-11 16:58:07 +0700

    update

--- app/src/main/java/com/example/projectbp2683/HomeActivity.kt
+++ app/src/main/java/com/example/projectbp2683/HomeActivity.kt
@@ -1,12 +1,33 @@
 package com.example.projectbp2683
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val btnBookKidActivity:Button = findViewById(R.id.buttonMenu1)
+        val rvBuku:RecyclerView = findViewById(R.id.recylerViewBuku)
+        //set layout manager di RecylerView
+        rvBuku.layoutManager = LinearLayoutManager(this)
+        //list data
+        val data = ArrayList<BukuModel>()
+        data.add(BukuModel(R.drawable.book1,"AlfandiBook1","Lorem Ipsum Dolor Sit Amet"))
+        //set adapter
+        val adapter=AdapterHome(data)
+        //set adapter ke recyler view
+        rvBuku.adapter = adapter
+
+        btnBookKidActivity.setOnClickListener{
+            val intentBookkid=Intent(this,BookKidActivity::class.java)
+            startActivity(intentBookkid)
+        }
 
     }
 }
